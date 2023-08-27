8c1ad5262b7638e6baa423d25b857f100f0c68c7
Ferdinand05 <fkoryanto@gmail.com>
2023-08-27 14:24:23 +0700

    add:dark mode, scroller,contact section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         poppins: ["Poppins"],
+      },
+      colors: {
+        darkmode: "#121212",
+        textdark: "#eeeeee",
       },
     },
   },
